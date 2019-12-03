68c315d9fda88b99a73aef8cfdd9e0f308d03c98
Briarwoodsz <johnjosephhoy@gmail.com>
2019-12-03 10:26:13 -0700

    created all the paths and the functions tied to them
    created all my relavent paths for creating, finding, deleting notes/articles for each path

--- server.js
+++ server.js
@@ -57,39 +57,132 @@ db.once("open", function () {
 ////////////////////
 
 app.get("/", (req, res) => {
-
+     Article.find({ "saved": false }, (err, data) => {
+          let hbsObject = {
+               article: data
+          };
+          console.log(hbsObject);
+          res.render("home", hbsObject);
+     });
 });
 
 app.get("/saved", (req, res) => {
-
+     Article.find({ "saved": true }).populate("notes").exe((err, articles) => {
+          let hbsObject = {
+               article: articles
+          };
+          res.render("saved", hbsObject);
+     });
 });
 
 app.get("/scrapper", (req, res) => {
+     request("https://www.nytimes.com/", (err, response, html) => {
+          let $ = cheerio.load(html);
+          $("artcile").each((i, element) => {
+               let result = {};
+
+               summary = ""
+               if ($(this).find("ul").length) {
+                    summary = $(this).find("li").first().text();
+               } else {
+                    summary $(this).find("p").text();
+               };
+
+               result.title = $(this).find("h2").text();
+               result.summary = summary;
+               result.link = `https://www.nytimes.com${$(this).find("a").attr("href")}`;
+          })
+     })
 
 });
 
 app.get("/articles", (res, res) => {
-
+     Article.find({}, (err, data) => {
+          if (err) {
+               console.log(err);
+          } else {
+               res.json(data);
+          }
+     });
 });
 
 app.get("/articles:id", (res, res) => {
-
+     Article.findOne({ "_id": req.params.id })
+          .populate("note")
+          .exec((err, data) => {
+               if (err) {
+                    console.log(err);
+               } else {
+                    res.json(data);
+               }
+          });
 });
 
 app.post("/articles/save:id", (res, res) => {
-
+     Article.findOneAndUpdate({ "_id": req.params.id }, {
+          "saved": true
+     })
+          .exec((err, data) => {
+               if (err) {
+                    console.log(err);
+               } else {
+                    res.send(data);
+               }
+          });
 });
 
 app.post("/articles/delete:id", (res, res) => {
-
+     Articles.findOneAndUpdate({ "_id": req.params.id }, { "saved": false, "notes": [] })
+          .exec((err, data) => {
+               if (err) {
+                    console.log(err);
+               } else {
+                    res.send(data);
+               }
+          });
 });
 
-app.post("/notes/saved:id", (req, res) => {
-
+app.post("/notes/save/:id", (req, res) => {
+     let newNote = new Note({
+          body: req.body.text,
+          article: req.params.id
+     });
+     console.log(req.body);
+     newNote.save((err, note) => {
+          if (err) {
+               console.log(err);
+          } else {
+               Article.findOneAndUpdate({ "_id": req.params.id }, { $push: { "notes": note } })
+                    .exec((err) => {
+                         if (err) {
+                              console.log(err);
+                              res.send(err)
+                         } else {
+                              res.send(note);
+                         }
+                    });
+          }
+     });
 });
 
 app.delete("notes/delete:id", (req, res) => {
-
+     Note.findOneAndRemove({ "_id": req.params.note_id },
+          (err) => {
+               if (err) {
+                    console.log(err);
+                    res.send(err);
+               } else {
+                    Article.findOneAndUpdate({ "_id": req.params.article_id }, { $pull: { "notes": req.params.note_id } })
+                         .exec((err) => {
+                              if (err) {
+                                   console.log(err);
+                                   res.send(err);
+                              } else {
+                                   res.send("Note Deleted");
+                              }
+                         });
+               }
+          });
 });
 ////////////////////
 // port
